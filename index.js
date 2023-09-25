be203dedc26f665225c40b2d823ced0aad7427fb
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-09-25 11:26:51 -0700

    complete

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Code your solutions in this file
-let countdown = 11
-while (countdown > 0) {
-    console.log(countdown--)
-}
+function writeCards(array, surprise) {
+    let arr = [];
+    for (let i = 0; i < array.length; i++) {
+      arr.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return arr;
+  }
+  console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "birthday"));
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num--);
+    }
+  }
+  countDown(10);
